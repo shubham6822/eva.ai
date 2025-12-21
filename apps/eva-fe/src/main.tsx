87ce3bae250659884f98fc6b81de5cc46727d199
Shubham <sharmashubham6822@gmail.com>
2025-12-21 23:33:49 +0530

    theme provider

--- apps/eva-fe/src/main.tsx
+++ apps/eva-fe/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "./provider/theme-provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
     <App />
-  </StrictMode>,
-)
+  </ThemeProvider>,
+);
