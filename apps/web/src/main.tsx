fdd879d435e2d11938244727d3454c192e35e977
Shubham <sharmashubham6822@gmail.com>
2025-12-22 16:25:05 +0530

    feat: shadcn sidebar

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./provider/theme-provider.tsx";
+import Layout from "./components/layout/sidebar/layout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </ThemeProvider>,
 );
