fa7b700bc213b562d55cb2b4b7c9324e3c484234
Shubham <sharmashubham6822@gmail.com>
2025-12-25 20:47:28 +0530

    feat: cors

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,18 @@
+import bodyParser from 'body-parser'
+import cors from "cors"
+import dotenv from 'dotenv'
 import express from 'express'
 
-const app : express.Application = express()
+const app: express.Application = express()
+
+dotenv.config()
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.json())
-
+app.use(cors())
 app.get('/', (_, res) => {
-  res.send('EVA Backend Running')
+  res.json({ message: 'API is running' })
 })
 
 export default app
